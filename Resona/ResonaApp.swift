78e4d3679f27a9c1e5102cd8f5d4e9c560e1dfad
Yizheng_J <147356902+Jyz922@users.noreply.github.com>
2026-03-30 22:18:39 -0400

    Initial Commit

---
+++ Resona/ResonaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResonaApp.swift
+//  Resona
+//
+//  Created by Yizheng Jiang on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct ResonaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
